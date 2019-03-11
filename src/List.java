60813e1216d8d2daea20a6321ba7ef1b0884d41f
Jeymsson <jeymsson@hotmail.com>
2019-03-11 17:11:48 -0300

    Conclusion of List.

--- src/List.java
+++ src/List.java
@@ -2,8 +2,9 @@
 public class List {
 
 	/* Variaveis */
-	Node head, tail;
-	int numNodes;
+	private Node head, tail;
+	private int numNodes;
+	private boolean log = false;
 	
 	/* Construtores */
 	public List() {
@@ -16,29 +17,104 @@ public class List {
 	public Node getHead() {
 		return this.head;
 	}
-	public void setHead(Node head) {
+	private Node setHead(Node head) {
 		this.head = head;
+		return getHead();
 	}
 	public Node getTail() {
 		return this.tail;
 	}
-	public void setTail(Node tail) {
+	private Node setTail(Node tail) {
 		this.tail = tail;
+		return getTail();
 	}
 	public int getNumNodes() {
 		return this.numNodes;
 	}
-	public void setNumNodes(int numNodes) {
+	private int setNumNodes(int numNodes) {
 		this.numNodes = numNodes;
+		return getNumNodes();
+	}
+	private <G> boolean sendLog(G value) {
+		if(getLog())
+			System.out.println(value);
+		return getLog();
+	}
+	public boolean getLog() {
+		return this.log;
+	}
+	public boolean setLog(boolean log) {
+		this.log = log;
+		return getLog();
 	}
 
 	/* Metodos */
-	boolean isEmpty() {
+	public boolean isEmpty() {
 		if (getNumNodes() == 0) {
 			return true;			
 		} else {
 			return false;
 		}
+	}
+	public <G> void pushI(G value) {
+		Node n;
+		if (isEmpty()) {
+			n = new Node(value);
+			setHead(n);
+			setTail(n);
+		} else {
+			n = new Node(value, getHead(), null);
+			getHead().setBack(n);
+			setHead(n);
+		}
+		setNumNodes(getNumNodes() +1);
+	}
+	public <G> void pushL(G value) {
+		Node n;
+		if (isEmpty()) {
+			n = new Node(value);
+			setHead(n);
+			setTail(n);
+		} else {
+			n = new Node(value, null, getTail());
+			getTail().setNext(n);
+			setTail(n);
+		}
+		setNumNodes(getNumNodes() +1);
+	}
+	public Node popI() {
+		Node ret = null;
+		if (isEmpty()) {
+			sendLog("List.popI: List is empty.");
+		} else if(getNumNodes() == 1){
+			ret = getHead();
+			setHead(null);
+			setTail(null);
+			setNumNodes(getNumNodes() -1);
+		} else {
+			ret = getHead();
+			setHead(getHead().getNext()).setBack(null);
+			ret.setNext(null);
+			setNumNodes(getNumNodes() -1);
+		}
+		return ret;
+	}
+	public Node popL() {
+		Node ret = null;
+		if (isEmpty()) {
+			sendLog("List.popL: List is empty.");
+		} else if(getNumNodes() == 1){
+			ret = getHead();
+			setHead(null);
+			setTail(null);
+			setNumNodes(getNumNodes() -1);
+		} else {
+			ret = getTail();
+			setTail(getTail().getBack()).setNext(null);
+			ret.setBack(null);
+			setNumNodes(getNumNodes() -1);
+		}
+		return ret;
 	}
 
 }
