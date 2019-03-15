ff8e29acced222ae336b8227bba5a324f24facbd
Jeymsson <jeymsson@hotmail.com>
2019-03-15 18:45:53 -0300

    + Methods of Edge.

--- src/Graph/Edge.java
+++ src/Graph/Edge.java
@@ -1,14 +1,46 @@
 package Graph;
 
+import Caracteristics.CaractEdge;
+
 public class Edge {
 
-	public Edge() {
+	/* Variaveis */
+	private CaractEdge info;
+	private Arrow setaO, setaD;
+
+	/* Construtores */
+	public Edge(Arrow origem, Arrow detino) {
 		// TODO Auto-generated constructor stub
+		setInfo(new CaractEdge());
+		setSetaO(origem);
+		setSetaD(detino);
+	}
+	public Edge(Vertice origem, Vertice detino) {
+		// TODO Auto-generated constructor stub
+		setInfo(new CaractEdge());
+		setSetaO(new Arrow.Builder().setVertice(origem).Build());
+		setSetaD(new Arrow.Builder().setVertice(detino).Build());
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	/* Getters e Setters */
+	public Arrow getSetaO() {
+		return this.setaO;
+	}
+	public void setSetaO(Arrow setaO) {
+		this.setaO = setaO;
+	}
+	public CaractEdge getInfo() {
+		return this.info;
+	}
+	public void setInfo(CaractEdge info) {
+		this.info = info;
+	}
+	public Arrow getSetaD() {
+		return this.setaD;
+	}
+	public void setSetaD(Arrow setaD) {
+		this.setaD = setaD;
 	}
 
 }
