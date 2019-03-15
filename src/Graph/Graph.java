3b003206b3b081a738590aea76677b1b320ca22f
Jeymsson <jeymsson@hotmail.com>
2019-03-15 18:58:50 -0300

    + Methods of Graph.

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,14 +1,49 @@
 package Graph;
 
+import List.List;
+import Caracteristics.CaractGraph;
+
 public class Graph {
 
+	/* Variaveis */
+	private CaractGraph info;
+	private Vertice vRoot;
+	private List vertices;
+	private List edges;
+
+	/* Construtores */
 	public Graph() {
 		// TODO Auto-generated constructor stub
+		setInfo(new CaractGraph());
+		setRoot(null);
+		setVertices(new List());
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/* Metodos */
+	/* Getters e Setters */
+	public List getVertices() {
+		return this.vertices;
+	}
+	public void setVertices(List vertices) {
+		this.vertices = vertices;
+	}
+	public CaractGraph getInfo() {
+		return this.info;
+	}
+	public void setInfo(CaractGraph info) {
+		this.info = info;
+	}
+	public Vertice getRoot() {
+		return this.vRoot;
+	}
+	public void setRoot(Vertice vRoot) {
+		this.vRoot = vRoot;
+	}
+	public List getEdges() {
+		return this.edges;
+	}
+	public void setEdges(List edges) {
+		this.edges = edges;
 	}
 
 }
