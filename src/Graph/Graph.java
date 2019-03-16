d178f6c066011bd0ddd850c8d9f70d7f28562a8d
Jeymsson <jeymsson@hotmail.com>
2019-03-15 22:59:45 -0300

    + Implementation of Graph methods.

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,7 +1,9 @@
 package Graph;
 
 import List.List;
+import List.Node;
 import Caracteristics.CaractGraph;
+import Caracteristics.Types;
 
 public class Graph {
 
@@ -10,6 +12,7 @@ public class Graph {
 	private Vertice vRoot;
 	private List allVertices;
 	private List allEdges;
+	private boolean log;
 
 	/* Construtores */
 	public Graph() {
@@ -18,9 +21,20 @@ public class Graph {
 		setRoot(null);
 		setAllVertices(new List());
 		setAllEdges(new List());
+		setLog(false);
 	}
 
 	/* Metodos */
+	private <G> boolean sendLog(G value) {
+		if(getLog())
+			System.out.println(value);
+		return getLog();
+	}
+	private <G> boolean sendLogs(G value) {
+		if(getLog())
+			System.out.print(value);
+		return getLog();
+	}
 	public boolean isEmpty() {
 		if(getInfo().getNumVertices() == 0) {
 			return true;
@@ -28,6 +42,64 @@ public class Graph {
 			return false;
 		}
 	}
+	public void setOrigin(Vertice v) {
+		if (v == null) {
+			sendLog("Graph.newVertice: Vertice empty.");
+		} else {
+			if (isEmpty()) {
+				setRoot(v);
+			}
+			getAllVertices().pushI(v);
+			
+			getInfo().setNumVertices(getInfo().getNumVertices() +1);
+		}
+	}
+	public Vertice getOrigin() {
+		return getRoot();
+	}
+	public void printAllVertices() {
+		Node temp = getAllVertices().getHead();
+		Vertice value = null;
+
+		value = (Vertice) temp.getObj();
+		sendLogs(value.getInfo().getValor());
+		while (temp.getNext() != null) {
+			temp = temp.getNext();
+			value = (Vertice) temp.getObj();
+			sendLogs(", "+ value.getInfo().getValor());
+		}
+		sendLog("");
+	}
+	public void printAllEdges() {
+		Node temp = getAllEdges().getHead();
+		Edge value = null;
+		while (temp.getNext().getNext() != null) {
+			value = (Edge) temp.getObj();
+			sendLogs(value.getInfo().getValor() +", ");
+			temp = temp.getNext();
+		}
+		value = (Edge) temp.getObj();
+		sendLog(value.getInfo().getValor());
+	}
+	public Vertice findVerticeValue(Object value) {
+		Node temp = getAllVertices().getHead();
+		Vertice vertTemp = null;
+		String vertValue = null;
+		while (temp != null) {
+			vertTemp = (Vertice) temp.getObj();
+			vertValue = (String) vertTemp.getInfo().getValor();
+			if(vertValue.equals((String)value)) {
+				break;
+			}
+			temp = temp.getNext();
+		}
+		if(vertValue.equals((String)value)) {
+			return vertTemp;
+		} else {
+			return null;			
+		}
+	}
+
 	/* Getters e Setters */
 	public List getAllVertices() {
 		return this.allVertices;
@@ -52,6 +124,12 @@ public class Graph {
 	}
 	public void setAllEdges(List edges) {
 		this.allEdges = edges;
+	}
+	public boolean getLog() {
+		return this.log;
+	}
+	public void setLog(boolean log) {
+		this.log = log;
 	}
 
 }
